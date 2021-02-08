7f009a202789215dc7a7be894a1378f741696401
danyaalqaramseh <d.alqaramseh98@gmial.com>
2021-02-09 01:35:29 +0200

    lab02ed

--- app.js
+++ app.js
@@ -3,42 +3,84 @@
 
 let ageOld =prompt('Do you think my age is 56 ? ?');
 
-if( ageOld.toLowerCase() === 'yes' || ageOld.toLowerCase()==='no' ) {
+if( ageOld.toLowerCase() === 'yes' || ageOld.toLowerCase()==='y' ) {
     console.log('test');
-    alert('My age is 22');
+    alert('No , My age is 22');
     console.log(ageOld);
  }
+ else if (ageOld.toLowerCase()=== 'no' || ageOld.toLowerCase()==='n' ){
+
+   console.log('test');
+   alert('you alright im not 56 im 22');
+
+
+ }
+
+
  let jobPref = prompt('Do you think i like Programming?');
- if(jobPref.toLowerCase() === 'yes' || jobPref.toLowerCase()==='no' ){
+ if(jobPref.toLowerCase() === 'yes' || jobPref.toLowerCase()==='y' ){
 
 
     console.log('test');
-    alert(' to be honest i dont like anything in life ');
+    alert(' yes i like programming ');
     console.log(jobPref);
+ }
+ else if (jobPref.toLowerCase()==='no' || jobPref.toLowerCase()==='n'){
+
+
+   console.log('test');
+   alert(' you\'re wrong i like programming  ');
+
  }
 
  let otaku=prompt('am i otaku ?  ');
- if( otaku.toLowerCase() === 'yes' || otaku.toLowerCase()==='no' ) {
+ if( otaku.toLowerCase() === 'yes' || otaku.toLowerCase()==='y' ) {
 
     console.log('test');
-    alert('to believe or not im an otaku and weeb also');
+    alert('you are otaku too because you know what means *-*');
     console.log(otaku);
+ }
+ else if (otaku.toLowerCase()==='no' || otaku.toLowerCase()==='n'){
+   console.log('test');
+
+  alert('are you kidding me ? even ubama knows im an otaku');
+
+
  }
 
  let offcialName=prompt('Do you think my name is Danya ?');
 
- if( offcialName.toLowerCase() === 'yes' || offcialName.toLowerCase()==='no' ) {
+ if( offcialName.toLowerCase() === 'yes' || offcialName.toLowerCase()==='y' ) {
 
     console.log('test');
-    alert('as you see im danya , we are coding here not playing');
+    alert('yes im danya what a suprise !!');
     console.log(offcialName);
 
  }
+else if (offcialName.toLowerCase()==='no'|| offcialName.toLowerCase()==='n'){
+
+alert('why you think that , im danya didnt know me yet!! ');
+console.log('test');
+
+}
+
+
+
  let knowMe=prompt(' Do you think you realy know me ??');
- if( knowMe.toLowerCase() === 'yes' || knowMe.toLowerCase()==='no' ) {
+ if( knowMe.toLowerCase() === 'yes' || knowMe.toLowerCase()==='y' ) {
 
     console.log('test');
-    alert('no one can know me :) ');
+    alert(' Liar , no one can know me  ');
     console.log(knowMe);
  }
+else if (knowMe.toLowerCase()==='no'||knowMe.toLowerCase()==='n'){
+
+alert('i cant blame you , i dont like talking a lot about my self');
+console.log('test');
+
+
+
+
+
+}
 alert('Welcome to Story of My Life' );
