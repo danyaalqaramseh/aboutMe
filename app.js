31d573181ef03e73dc7f1410e868a1937f4f7ebf
danyaalqaramseh <d.alqaramseh98@gmial.com>
2021-02-08 17:05:05 +0200

    lab02

---
+++ app.js
@@ -0,0 +1,44 @@
+
+'use strict';
+
+let ageOld =prompt('Do you think my age is 56 ? ?');
+
+if( ageOld.toLowerCase() === 'yes' || ageOld.toLowerCase()==='no' ) {
+    console.log('test');
+    alert('My age is 22');
+    console.log(ageOld);
+ }
+ let jobPref = prompt('Do you think i like Programming?');
+ if(jobPref.toLowerCase() === 'yes' || jobPref.toLowerCase()==='no' ){
+
+
+    console.log('test');
+    alert(' to be honest i dont like anything in life ');
+    console.log(jobPref);
+ }
+
+ let otaku=prompt('am i otaku ?  ');
+ if( otaku.toLowerCase() === 'yes' || otaku.toLowerCase()==='no' ) {
+
+    console.log('test');
+    alert('to believe or not im an otaku and weeb also');
+    console.log(otaku);
+ }
+
+ let offcialName=prompt('Do you think my name is Danya ?');
+
+ if( offcialName.toLowerCase() === 'yes' || offcialName.toLowerCase()==='no' ) {
+
+    console.log('test');
+    alert('as you see im danya , we are coding here not playing');
+    console.log(offcialName);
+
+ }
+ let knowMe=prompt(' Do you think you realy know me ??');
+ if( knowMe.toLowerCase() === 'yes' || knowMe.toLowerCase()==='no' ) {
+
+    console.log('test');
+    alert('no one can know me :) ');
+    console.log(knowMe);
+ }
+alert('Welcome to Story of My Life' );
---
+++ blah.md
@@ -0,0 +1,5 @@
+let ageOld =prompt('Do you think my age is 56 ? ?');
+let jobPref = prompt('Do you think i like Programming?');
+let otaku=prompt('am i otaku ?  ');
+let offcialName=prompt('Do you think my name is Danya ?');
+let knowMe=prompt(' Do you think you realy know me ??');
