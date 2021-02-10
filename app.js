15e5bebf7f8ec1ed937dcd082923ca68b61e6594
danyaalqaramseh <d.alqaramseh98@gmial.com>
2021-02-10 09:24:17 +0200

    lab2+3

--- app.js
+++ app.js
@@ -1,89 +1,89 @@
 
 'use strict';
 
-// let ageOld =prompt('Do you think my age is 56 ? ?');
+let ageOld =prompt('Do you think my age is 56 ? ?');
 
-// if( ageOld.toLowerCase() === 'yes' || ageOld.toLowerCase()==='y' ) {
-//     console.log('test');
-//     alert('No , My age is 22');
-//     console.log(ageOld);
-//  }
-//  else if (ageOld.toLowerCase()=== 'no' || ageOld.toLowerCase()==='n' ){
+if( ageOld.toLowerCase() === 'yes' || ageOld.toLowerCase()==='y' ) {
+    console.log('test');
+    alert('No , My age is 22');
+    console.log(ageOld);
+ }
+ else if (ageOld.toLowerCase()=== 'no' || ageOld.toLowerCase()==='n' ){
 
-//    console.log('test');
-//    alert('you alright im not 56 im 22');
-
-
-//  }
+   console.log('test');
+   alert('you alright im not 56 im 22');
 
 
-//  let jobPref = prompt('Do you think i like Programming?');
-//  if(jobPref.toLowerCase() === 'yes' || jobPref.toLowerCase()==='y' ){
+ }
 
 
-//     console.log('test');
-//     alert(' yes i like programming ');
-//     console.log(jobPref);
-//  }
-//   else if (jobPref.toLowerCase()==='no' || jobPref.toLowerCase()==='n'){
+ let jobPref = prompt('Do you think i like Programming?');
+ if(jobPref.toLowerCase() === 'yes' || jobPref.toLowerCase()==='y' ){
 
 
-//    console.log('test');
-//    alert(' you\'re wrong i like programming  ');
-
-//  }
-
-//  let otaku=prompt('am i otaku ?  ');
-//  if( otaku.toLowerCase() === 'yes' || otaku.toLowerCase()==='y' ) {
-
-//     console.log('test');
-//     alert('you are otaku too because you know what means *-*');
-//     console.log(otaku);
-//  }
-//  else if (otaku.toLowerCase()==='no' || otaku.toLowerCase()==='n'){
-//    console.log('test');
-
-//   alert('are you kidding me ? even ubama knows im an otaku');
+    console.log('test');
+    alert(' yes i like programming ');
+    console.log(jobPref);
+ }
+  else if (jobPref.toLowerCase()==='no' || jobPref.toLowerCase()==='n'){
 
 
-//  }
+   console.log('test');
+   alert(' you\'re wrong i like programming  ');
 
-//  let offcialName=prompt('Do you think my name is Danya ?');
+ }
 
-//  if( offcialName.toLowerCase() === 'yes' || offcialName.toLowerCase()==='y' ) {
+ let otaku=prompt('am i otaku ?  ');
+ if( otaku.toLowerCase() === 'yes' || otaku.toLowerCase()==='y' ) {
 
-//     console.log('test');
-//     alert('yes im danya what a suprise !!');
-//     console.log(offcialName);
+    console.log('test');
+    alert('you are otaku too because you know what means *-*');
+    console.log(otaku);
+ }
+ else if (otaku.toLowerCase()==='no' || otaku.toLowerCase()==='n'){
+   console.log('test');
 
-//  }
-// else if (offcialName.toLowerCase()==='no'|| offcialName.toLowerCase()==='n'){
-
-// alert('why you think that , im danya didnt know me yet!! ');
-// console.log('test');
-
-// }
-
+  alert('are you kidding me ? even ubama knows im an otaku');
 
 
-//  let knowMe=prompt(' Do you think you realy know me ??');
-//  if( knowMe.toLowerCase() === 'yes' || knowMe.toLowerCase()==='y' ) {
+ }
 
-//     console.log('test');
-//     alert(' Liar , no one can know me  ');
-//     console.log(knowMe);
-//  }
-// else if (knowMe.toLowerCase()==='no'||knowMe.toLowerCase()==='n'){
+ let offcialName=prompt('Do you think my name is Danya ?');
 
-// alert('i cant blame you , i dont like talking a lot about my self');
-// console.log('test');
+ if( offcialName.toLowerCase() === 'yes' || offcialName.toLowerCase()==='y' ) {
+
+    console.log('test');
+    alert('yes im danya what a suprise !!');
+    console.log(offcialName);
+
+ }
+else if (offcialName.toLowerCase()==='no'|| offcialName.toLowerCase()==='n'){
+
+alert('why you think that , im danya didnt know me yet!! ');
+console.log('test');
+
+}
 
 
 
+ let knowMe=prompt(' Do you think you realy know me ??');
+ if( knowMe.toLowerCase() === 'yes' || knowMe.toLowerCase()==='y' ) {
+
+    console.log('test');
+    alert(' Liar , no one can know me  ');
+    console.log(knowMe);
+ }
+else if (knowMe.toLowerCase()==='no'||knowMe.toLowerCase()==='n'){
+
+alert('i cant blame you , i dont like talking a lot about my self');
+console.log('test');
 
 
-// }
-// alert('Welcome to Story of My Life' );
+
+
+
+}
+alert('Welcome to Story of My Life' );
 
 
 // lab03
